6febcedfae8cd24c959a5049073556c71110591e
Bacorsx <darcus_cs@hotmail.cl>
2025-08-29 11:29:53 -0400

    feat(categoria): CRUD Categoria

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    
+
+
+class Dispositivo(models.Model):
+    nombre = models.CharField(max_length=100)
+    consumo = models.IntegerField()
+    estado = models.BooleanField(default=True)
+    zona = models.ForeignKey(Zona, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
