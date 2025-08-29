185092a8e0325076602e62cab013961a920eaf34
Bacorsx <darcus_cs@hotmail.cl>
2025-08-29 17:52:50 -0400

    feat: crear tabla admin IC

--- monitoreo/dispositivos/admin.py
+++ monitoreo/dispositivos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Zona, Categoria, Dispositivo
 
-# Register your models here.
+admin.site.register([Categoria, Zona])
+admin.site.register(Dispositivo)
+
+@admin.register(Zona)
+class ZonaAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "ubicacion", "created_at", "updated_at","deleted_at")
+    search_fields = ("nombre", "ubicacion")
--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
+class BaseMode(models.Model):
+    ESTADOS = [
+        ("ACTIVO", "Activo"),
+        ("INACTIVO", "Inactivo")
+    ]
+
+    estado = models.CharField(max_length=10, choices=ESTADOS, default="ACTIVO")
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    deleted_at = models.DateField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+    
+class Zona(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Dispositivo(models.Model):
+    nombre = models.CharField(max_length=100)
+    consumo = models.IntegerField()
+    estado = models.BooleanField(default=True)
+    zona = models.ForeignKey(Zona, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
